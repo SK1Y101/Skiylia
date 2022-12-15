b1fba4047db615d0b67c71b3c0e7e6a70747dc29
Jack Lloyd-Walters <jack.lloyd-walters@canonical.com>
2022-12-15 00:20:35 +0000

    fix?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,11 +21,11 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx_tabs.tabs',
-    'KerboscriptLexer',
+    'Skiylia2Lexer',
 ]
 
 pygments_style = 'sphinx'
-highlight_language = 'python'
+highlight_language = 'skiylia2'
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
