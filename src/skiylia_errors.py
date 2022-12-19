26ac021659d324c2ffbb373590d6b8a918c6f877
Skye Owen-Lloyd-Walters <jack.lloyd-walters@canonical.com>
2022-12-19 02:34:06 +0000

    lexer that's a little more powerful

--- src/skiylia_errors.py
+++ src/skiylia_errors.py
@@ -19,7 +19,9 @@ class SkiyliaError(Exception):
         self.row = row
         self.message = message
         self.location = location
-        self.err = self.__class__.__name__
+        self.err = "".join(
+            [" " + x if x.isupper() else x for x in self.__class__.__name__]
+        )
 
     def __str__(self) -> str:
         return (
@@ -32,7 +34,7 @@ class SkiyliaError(Exception):
 class UnidentifiedCharacter(SkiyliaError):
     """Raised when an unknown character is found in the filestream."""
 
-    def __init__(self, column: int = 0, row: int = 0, char: str = "") -> None:
+    def __init__(self, column: int = 0, row: int = 0, char: str = "", *args) -> None:
         super().__init__(column, row, f"'{char}'")
 
 
@@ -40,3 +42,24 @@ class UnexpectedCharacter(SkiyliaError):
     """Raised when an unexpected character occurs in the filestream."""
 
     errcode = 1
+
+
+class UnterminatedClosure(SkiyliaError):
+    """Used when an opening literal is not closed."""
+
+    def __init__(self, column: int = 0, row: int = 0, char: str = "", *args) -> None:
+        antichar = {
+            "(": ")",
+            "[": "]",
+            "{": "}",
+        }.get(char, char)
+        quote = "'" if antichar != "'" else "`"
+        super().__init__(column, row, f"Missing {quote}{char}{quote}")
+
+
+class UnterminatedString(UnterminatedClosure):
+    """Raised when a string is not terminated correctly."""
+
+
+class UnterminatedComment(UnterminatedClosure):
+    """Raised when a comment is not terminated correctly."""
