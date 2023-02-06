2c7a9f28f6ba81c96d460854fd31d5fc44f22fb0
Jack Lloyd-Walters <jack.lloyd-walters@canonical.com>
2023-02-06 23:18:04 +0000

    new: introduce changelog and changelog workflow

--- src/skiylia.py
+++ src/skiylia.py
@@ -19,7 +19,7 @@ class Skiylia:
         # Fix bugs or issues (backwards compatible by definition)
         patch = 0
         # Build number (commits since initial tracks approximate age)
-        build = 98
+        build = 99
         # Additional identifiers (ie: alpha)
         ident = "pre-alpha"
 
