5c397914960a6cc809026bf4e1d97cf6efe5b3d1
Skye Owen-Lloyd-Walters <jack.lloyd-walters@canonical.com>
2023-02-10 22:18:50 +0000

    fix more?

--- src/skiylia.py
+++ src/skiylia.py
@@ -19,7 +19,7 @@ class Skiylia:
         # Fix bugs or issues (backwards compatible by definition)
         patch = 0
         # Build number (commits since initial tracks approximate age)
-        build = 165
+        build = 168
         # Additional identifiers (ie: alpha)
         ident = "pre-alpha"
 
